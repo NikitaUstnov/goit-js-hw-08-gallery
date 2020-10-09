c6332ac85a90cfa58608e8b1a1f95897526c4eb1
Nikita Ustinov <65974693+NikitaUstnov@users.noreply.github.com>
2020-10-09 16:21:44 +0300

    ОЙОЙОЙОЙ

--- js/my-genius-code.js
+++ js/my-genius-code.js
@@ -1,3 +1,22 @@
-import imgArr from "./gallery-items";
+import imgArr from "./gallery-items.js";
 
-console.log(imgArr);
+console.log(...imgArr);
+
+const imagesGridEl = imgArr.map(({ preview, original, description }) => {
+  return `<li class="gallery__item>
+  <a class="gallety__link> href="${original}">
+  <img class="gellery__image"
+  src="${preview}"
+  alt="${description}"
+  data-source="${original}"
+  </a>
+  </li>
+    "`;
+}, "");
+// +380994807984;
+
+console.log(imagesGridEl);
+
+const galleryEl = document.querySelector(".js-gallery");
+galleryEl.insertAdjacentHTML("beforebegin", imagesGridEl);
+ГАВНО КАКОЕ ТО
