a9b11f59755340e46081bec115c255e2d5326708
Nikita Ustinov <65974693+NikitaUstnov@users.noreply.github.com>
2020-10-10 00:35:48 +0300

    2313123

--- js/my-genius-code.js
+++ js/my-genius-code.js
@@ -1,21 +1,46 @@
 import imgArr from "./gallery-items.js";
 
-console.log(...imgArr);
-
 const imagesGridEl = imgArr.map(({ preview, original, description }) => {
-  return `<li class="gallery__item>
-  <a class="gallety__link> href="${original}">
+  return `<li class="gallery__item">
+  <a class="gallery__link">
   <img class="gallery__image"
   src="${preview}"
   alt="${description}"
-data-source="${original}"/>
+  data-source="${original}"
+  />
   </a>
-  </li>
-    "`;
-}, "");
-
-console.log(imagesGridEl);
-
+  </li>`
+}).join('');
+// 
 const galleryEl = document.querySelector(".js-gallery");
-imagesGridEl.insertAdjacentHTML("beforebegin", galleryEl);
+galleryEl.addEventListener('click', openModal)
+galleryEl.insertAdjacentHTML("beforeend", imagesGridEl);
   
+function openModal(evt) {
+  if (evt.target.nodeName !== "IMG") {
+    return
+  }
+      console.log(evt.target.alt)
+}
+
+
+
+
+const modalEl = document.querySelector(".js-lightbox")
+const closeButtonModalEl = document.querySelector('.lightbox__button')
+
+closeButtonModalEl.addEventListener('click', closeModal)
+function closeModal() {
+ modalEl.classList.remove('.is-open')
+}
+
+
+
+// const listImgEl = document.querySelector('.gallery__item')
+// listImgEl.addEventListener("click", openModal)
+
+// function openModal() {
+//  modalEl.classList.add("is-open")
+  
+// }
+
